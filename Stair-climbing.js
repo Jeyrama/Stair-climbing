4e0ed81de5d65087c5b7798cb675af72223f410d
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-12-17 15:31:33 -0800

    Update Stair-climbing.js

--- Stair-climbing.js
+++ Stair-climbing.js
@@ -48,3 +48,25 @@ function climbingStairs(cost) {
 }
 
 // or
+
+function climbingStairs(cost) {
+  
+  //you can start at first or second step 
+  //if 2 steps return lower cost
+  let n = cost.length;
+  if (n <= 2){
+    return Math.min(...cost);
+  }
+  //create an array storing minimum cost to reach each step 
+  
+  let minCost = new Array(n);
+  minCost[0] = cost[0];
+  minCost[1] = cost[1];
+  //we set minC 0 and 1 above so now we need to set minC[i] where i is each step of cost.
+  //minCost[i] is decided by adding cost of each step and either the step 1 or 2 prior whichever is cheaper
+  for (let i =2; i < n; i++){
+    minCost[i] = cost[i] + Math.min(minCost[i-1], minCost[i-2]);
+    
+  }//can exit off last step or second last whichever has less cost 
+  return Math.min(minCost[n-1], minCost[n-2]);
+}
